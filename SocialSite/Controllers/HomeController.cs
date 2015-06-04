1864d2610d2b1c4bbb1689ece0bcaa8cb3d46178
Dia Brown <dbrown1903@outlook.com>
2015-06-04 13:12:31 -0600

    The Entire Project
    Not completed - theme 2 for the profile page

---
+++ SocialSite/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SocialSite.Models;
+using System.IO;
+
+namespace SocialSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult ProfileThemeTwo()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            var gal = new ImageDisplayMod()
+            {
+                Images = Directory.EnumerateFiles(Server.MapPath("~/Images/GalleryImgs"))
+                .Select(fn => "~/Images/GalleryImgs/" + Path.GetFileName(fn))
+            };
+            return View(gal);
+        }
+    }
+}
